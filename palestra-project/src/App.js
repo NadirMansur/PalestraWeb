25a6e13b0bcf920fe61f892aee74323e9860f3f9
NadirMansur <nadirmansur89@gmail.com>
2025-04-02 23:10:31 -0300

    fix: delate fixed header

--- palestra-project/src/App.js
+++ palestra-project/src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState } from "react";
+import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import logo from "./assets/logo-sin-fondo.png";
 import Footer from "./components/Footer/Footer.jsx";
@@ -10,13 +10,9 @@ import { Contact } from "./pages/Contact/Contact.jsx";
 import HomePage from "./pages/HomePage/HomePage.tsx";
 
 const App = () => {
-  const [headerHeight, setHeaderHeight] = useState(0);
-
   return (
     <Router>
       <LockHeader
-        onHeightChange={(height) => setHeaderHeight(height)}
-        fixed
         slideInDown
         marca={
           <AvatarPerfil
@@ -28,15 +24,9 @@ const App = () => {
         }
       />
       <Routes>
-        <Route path='/' element={<HomePage headerHeight={headerHeight} />} />
-        <Route
-          path='/about'
-          element={<AboutPage headerHeight={headerHeight} />}
-        />
-        <Route
-          path='/contact'
-          element={<Contact headerHeight={headerHeight} />}
-        />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
--- palestra-project/src/components/LockHeader/index.tsx
+++ palestra-project/src/components/LockHeader/index.tsx
@@ -1,63 +1,24 @@
 import classNames from "classnames";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import "../../animation.css";
 import { Menu } from "../menu/index.tsx";
 import styles from "./LockHeader.module.scss";
 
 const LockHeader = ({
-  onHeightChange = undefined,
   slideInDown = false,
   fadeIn = false,
   lightSpeedIn = false,
-  fixed = false,
   marca,
 }: {
   marca: React.ReactNode;
   slideInDown?: boolean;
   fadeIn?: boolean;
   lightSpeedIn?: boolean;
-  fixed?: boolean;
-  onHeightChange?: (height: number) => void | null;
 }) => {
-  const [isSmall, setIsSmall] = useState(false);
-  const headerRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      // Cambia el estado dependiendo de la posición del scroll
-      if (window.scrollY > 50) {
-        setIsSmall(true);
-      } else {
-        setIsSmall(false);
-      }
-    };
-
-    // Escucha el evento de scroll
-    window.addEventListener("scroll", handleScroll);
-
-    const updateHeaderHeight = () => {
-      if (headerRef.current) {
-        onHeightChange && onHeightChange(headerRef.current.offsetHeight); // Envía la altura al padre
-      }
-    };
-
-    updateHeaderHeight(); // Llama al montar
-    window.addEventListener("resize", updateHeaderHeight); // Recalcula al redimensionar
-
-    // Limpia el evento al desmontar el componente
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      window.removeEventListener("resize", updateHeaderHeight);
-    };
-  }, [onHeightChange]);
-
   return (
     <header
-      ref={headerRef}
       className={classNames(
         styles.header,
-        fixed && styles.fixed,
-        { [styles.small]: isSmall && fixed },
         slideInDown && "slideInDown",
         fadeIn && "fadeIn",
         lightSpeedIn && "lightSpeedIn",
